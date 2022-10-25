bb381d9896ae66b7cc73a811764094fa21c8c03a
“Suraj <suraj.karki500@protonmail.com>
2022-10-25 20:05:24 +0545

    Created basic express graphql server

---
+++ server/app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const { graphqlHTTP } =  require("express-graphql");
+
+const schema = require("./schema/schema");
+
+const app = express();
+
+app.use("/graphql", graphqlHTTP({ schema }));
+
+app.listen(5000, () => console.log("Listening on PORT 5000"));
---
+++ server/schema/schema.js
@@ -0,0 +1,28 @@
+const graphql  = require("graphql");
+
+const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+
+const BookType = new GraphQLObjectType({
+  name: "Book",
+  fields: () => ({
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    genre: { type: GraphQLString },
+  }),
+});
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  fields: {
+    book: {
+      type: BookType,
+      args: { id: { type: GraphQLString } },
+      resolve(parent, args) {
+        // Code to get data from db/other sources
+      },
+    },
+  },
+});
+
+module.exports =  new GraphQLSchema({
+  query: RootQuery,
+});;
