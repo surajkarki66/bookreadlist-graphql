61c4d415f0ad6228fabe3b564d72ed9256d63915
“Suraj <suraj.karki500@protonmail.com>
2022-10-26 19:11:24 +0545

    Created BookList component

---
+++ client/src/components/BookList.jsx
@@ -0,0 +1,29 @@
+import { gql, useQuery } from "@apollo/client";
+
+const getBooksQuery = gql`
+  {
+    books {
+      name
+      id
+    }
+  }
+`;
+
+const BookList = () => {
+  const { loading, data } = useQuery(getBooksQuery);
+  const displayBooks = () => {
+    if (loading) {
+      return <div>Loading books...</div>;
+    } else {
+      return data.books.map((book) => {
+        return <li key={book.id}>{book.name}</li>;
+      });
+    }
+  };
+  return (
+    <div>
+      <ul id="book-list">{displayBooks()}</ul>
+    </div>
+  );
+};
+export default BookList;
