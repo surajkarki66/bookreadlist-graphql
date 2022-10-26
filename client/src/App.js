a43eaeede3d1aec35d5e1d2af0ab500df210103a
“Suraj <suraj.karki500@protonmail.com>
2022-10-26 19:11:58 +0545

    Configure Apollo Client

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,10 @@
+import BookList from "./components/BookList";
+
 function App() {
   return (
     <div id="main">
       <h1>Book Reading List</h1>
+      <BookList />
     </div>
   );
 }
--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+const client = new ApolloClient({
+  uri: "http://localhost:5000/graphql",
+  cache: new InMemoryCache(),
+});
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>
 );
 
