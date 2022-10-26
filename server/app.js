2d8b27415af31b9eca0f2e201afb02322bee0128
“Suraj <suraj.karki500@protonmail.com>
2022-10-26 16:37:12 +0545

    Modified graphql server

--- server/app.js
+++ server/app.js
@@ -1,10 +1,10 @@
 const express = require("express");
-const { graphqlHTTP } =  require("express-graphql");
+const { graphqlHTTP } = require("express-graphql");
 
 const schema = require("./schema/schema");
 
 const app = express();
 
-app.use("/graphql", graphqlHTTP({ schema }));
+app.use("/graphql", graphqlHTTP({ schema, graphiql: true }));
 
 app.listen(5000, () => console.log("Listening on PORT 5000"));
