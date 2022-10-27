6b07f1c16d18122ffaf5b6f8330dd7d4ec9a442a
“Suraj <suraj.karki500@protonmail.com>
2022-10-27 20:08:49 +0545

    Final commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import BookList from "./components/BookList";
+import AddBook from "./components/AddBook";
 
 function App() {
   return (
     <div id="main">
       <h1>Book Reading List</h1>
       <BookList />
+      <AddBook />
     </div>
   );
 }
