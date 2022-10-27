7c6bbc469fd7df6a4cb3c1d26a68dfb4bd73d1e8
“Suraj <suraj.karki500@protonmail.com>
2022-10-27 20:08:14 +0545

    Modified BookList component

--- client/src/components/BookList.jsx
+++ client/src/components/BookList.jsx
@@ -1,28 +1,29 @@
-import { gql, useQuery } from "@apollo/client";
+import { useState } from "react";
+import { useQuery } from "@apollo/client";
 
-const getBooksQuery = gql`
-  {
-    books {
-      name
-      id
-    }
-  }
-`;
+import { getBooksQuery } from "../queries/queries";
+import BookDetails from "./BookDetails";
 
 const BookList = () => {
+  const [selected, setSelected] = useState(null);
   const { loading, data } = useQuery(getBooksQuery);
   const displayBooks = () => {
     if (loading) {
       return <div>Loading books...</div>;
     } else {
-      return data.books.map((book) => {
-        return <li key={book.id}>{book.name}</li>;
+      return data?.books.map((book) => {
+        return (
+          <li key={book.id} onClick={(e) => setSelected(book.id)}>
+            {book.name}
+          </li>
+        );
       });
     }
   };
   return (
     <div>
       <ul id="book-list">{displayBooks()}</ul>
+      <BookDetails bookId={selected} />
     </div>
   );
 };
